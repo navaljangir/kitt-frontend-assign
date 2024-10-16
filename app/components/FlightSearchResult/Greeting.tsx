169d5ad3ee49e8878a2c129e416b380fd3fc0d04
Naval Kishor <jangirnaval392003@gmail.com>
2024-10-16 14:31:27 +0530

    FixedGreeting

--- app/components/FlightSearchResult/Greeting.tsx
+++ app/components/FlightSearchResult/Greeting.tsx
@@ -1,20 +1,22 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function GetGreeting(){
     const name = 'Brian';
     const [greet , setGreeting] = useState('Good Morning')
-    const currTime = new Date();
-    const hours = currTime.getHours()
-    console.log(hours);
-    if (hours >= 0 && hours < 12) {
-        setGreeting('Good Morning')
-    } else if (hours >= 12 && hours < 17) {
-        setGreeting('Good Afternoon')
-    } else if (hours >= 17 && hours <= 23) {
-        setGreeting('Good Evening')
-    }
+    useEffect(()=>{
+        const currTime = new Date();
+        const hours = currTime.getHours()
+        if (hours >= 0 && hours < 12) {
+            setGreeting('Good Morning')
+        } else if (hours >= 12 && hours < 17) {
+            setGreeting('Good Afternoon')
+        } else if (hours >= 17 && hours <= 23) {
+            setGreeting('Good Evening')
+        }
+        
+    },[greet , setGreeting])
     return <div className="flex justify-center text-7xl mt-28 mb-10">
     {greet} {name}
 </div>
