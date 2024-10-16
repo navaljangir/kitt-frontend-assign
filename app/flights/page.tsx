cdc155e5a5e49415246ced73458538e4b5fbe6d9
Naval Kishor <jangirnaval392003@gmail.com>
2024-10-16 14:29:14 +0530

    fixedGreeting

--- app/flights/page.tsx
+++ app/flights/page.tsx
@@ -1,24 +1,12 @@
 
 import { FlightCompMain } from "../components/FlightCompMain";
+import { GetGreeting } from "../components/FlightSearchResult/Greeting";
 
 export default function flightHomePage() {
-    const name = 'Brian';
-    let greet = "Good Morning"
-    const currTime = new Date();
-    const hours = currTime.getHours()
-    console.log(hours);
-    if (hours >= 0 && hours < 12) {
-        greet = 'Good Morning'
-    } else if (hours >= 12 && hours < 17) {
-        greet = 'Good Afternoon'
-    } else if (hours >= 17 && hours <= 23) {
-        greet = 'Good Evening'
-    }
+    
     return (
         <div className="flex flex-col ">
-            <div className="flex justify-center text-7xl mt-28 mb-10">
-                {greet} {name}
-            </div>
+            <GetGreeting/>
             <div className="flex justify-center">
                 <div className="border-2 rounded-lg py-6 px-7">
                     <div className="bg-[#F5F7FA] w-32 rounded-md px-3 py-2 flex justify-center">
